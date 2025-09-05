81bf75b0a426641abbe2da53206accf2637d2d6f
unknown <tamer12465@gmail.com>
2025-09-05 09:09:20 +0300

    Add Suggestion Endpoint

--- SoftwareCompaniesManagement.Api/MapEndpoints/SuggestionEndpoints.cs
+++ SoftwareCompaniesManagement.Api/MapEndpoints/SuggestionEndpoints.cs
@@ -1,4 +1,7 @@
-﻿using SoftwareCompaniesManagement.Api.Data;
+﻿using AutoMapper;
+using SoftwareCompaniesManagement.Api.Data;
+using SoftwareCompaniesManagement.Api.DTO.GetDto;
+using SoftwareCompaniesManagement.Model;
 
 namespace SoftwareCompaniesManagement.Api.MapEndpoints
 {
@@ -25,11 +28,46 @@ namespace SoftwareCompaniesManagement.Api.MapEndpoints
         {
             var suggestionGroup = app.MapGroup("companies/{companyId}/projects/{projectId}/suggest");
 
+            var mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Developer, DeveloperDto>();
+                cfg.CreateMap<DeveloperDto, Developer>();
+            });
+
+            var developerMapper = mapperConfiguration.CreateMapper();
+
             suggestionGroup.MapGet("", (CompaniesContext dbContext, HttpContext httpContext, int companyId, int projectId) =>
             {
-                var technologies = dbContext.ProjectTechnologies.Where(pt => pt.ProjectId == projectId).Select(pt => pt.TechnologyId);
-                var developerTechnologies = dbContext.DeveloperTechnologies.Where(dt => technologies.Contains(dt.TechnologyId));
+                var token = TokenDecoder.DecodeToken(httpContext);
 
+                if (token is null)
+                {
+                    return Results.Unauthorized();
+                }
+
+                var role = token.FindFirst("_role").Value;
+                var sentCompanyId = token.FindFirst("_companyId").Value;
+                var infoId = token.FindFirst("_infoId").Value;
+
+                if (role != "company" && role != "project_manager" || int.Parse(sentCompanyId) != companyId)
+                {
+                    return Results.Unauthorized();
+                }
+
+                if(role == "project_manager" && dbContext.Projects.Where(project => project.Id == projectId).Select(project => project.ManagerId).First() != int.Parse(infoId))
+                {
+                    return Results.Unauthorized();
+                }
+
+                var technologies = dbContext.ProjectTechnologies.Where(pt => pt.ProjectId == projectId).Select(pt => pt.TechnologyId);
+                var developers = dbContext.DeveloperTechnologies.Where(dt => technologies.Contains(dt.TechnologyId))
+                    .GroupBy(dt => dt.DeveloperId)
+                    .Select(g => new { Id = g.Key, AvgPoints = g.Average(dt => 0.4 * dt.ExperienceYears + 0.6 * experienceMapping(dt.ExperienceLevel)) })
+                    .OrderByDescending(g => g.AvgPoints)
+                    .Select(g => g.Id)
+                    .Select(id => dbContext.Developers.Find(id));
+
+                return Results.Ok(developers.Select(developerMapper.Map<Developer, DeveloperDto>));
             });
 
             return suggestionGroup;
--- SoftwareCompaniesManagement.Api/obj/Debug/net8.0/SoftwareCompaniesManagement.Api.AssemblyInfo.cs
+++ SoftwareCompaniesManagement.Api/obj/Debug/net8.0/SoftwareCompaniesManagement.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftwareCompaniesManagement.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a51c4f03fadc9701368dc1f65cf0b894d2393951")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+186c8e14153c0fb6350ea1c6397d6b322a8bfe57")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftwareCompaniesManagement.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftwareCompaniesManagement.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
